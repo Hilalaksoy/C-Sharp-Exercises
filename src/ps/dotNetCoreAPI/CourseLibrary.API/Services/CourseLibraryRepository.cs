25621d9c86caade4bed0fcfd9ec4183759b4666e
Hilal Aksoy <hll.aksy29@gmail.com>
2020-12-08 14:06:35 +0300

    first commit

---
+++ src/ps/dotNetCoreAPI/CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Entities;
+
+namespace CourseLibrary.API.Services
+{
+    public class CourseLibraryRepository: ICourseLibraryRepository
+    {
+        private readonly CourseLibraryContext _context;
+
+        public CourseLibraryRepository(CourseLibraryContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public Task<IEnumerable<Course>> GetCoursesAsync(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Course> GetCourseAsync(Guid authorId, Guid courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCourse(Guid authorId, Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCourse(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Author>> GetAuthorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Author>> GetAuthorsWithIdsAsync(IEnumerable<Guid> authorsIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public PagedList<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public Task<Author> GetAuthorAsync(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Author>> GetAuthorsAsync(IEnumerable<Guid> authorsIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAuthorRange(IEnumerable<Author> author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AuthorExistsAsync(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ps/dotNetCoreAPI/CourseLibrary.API/Services/ICourseLibraryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseLibrary.API.Entities;
+
+
+namespace CourseLibrary.API.Services
+{
+    public interface ICourseLibraryRepository
+    {
+        Task<IEnumerable<Course>> GetCoursesAsync(Guid authorId);
+        Task<Course> GetCourseAsync(Guid authorId, Guid courseId);
+        void AddCourse(Guid authorId, Course course);
+        void UpdateCourse(Course course);
+        void DeleteCourse(Course course);
+        Task<IEnumerable<Author>> GetAuthorsAsync();
+        Task<IEnumerable<Author>> GetAuthorsWithIdsAsync(IEnumerable<Guid> authorsIds);
+        //PagedList<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters);
+        Task<Author> GetAuthorAsync(Guid authorId);
+        Task<IEnumerable<Author>> GetAuthorsAsync(IEnumerable<Guid> authorsIds);
+        void AddAuthor(Author author);
+        void AddAuthorRange(IEnumerable<Author> author);
+        void DeleteAuthor(Author author);
+        void UpdateAuthor(Author author);
+        Task<bool> AuthorExistsAsync(Guid authorId);
+        Task<bool> SaveAsync();
+
+    }
+}
